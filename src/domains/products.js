6975dab8235fc3aaf74e2b340ecf36d3f1d6fadb
Kye <NovusAa12@proton.me>
2024-02-19 16:45:37 +0000

    update getAllProductsDb to include catergory

--- src/domains/products.js
+++ src/domains/products.js
@@ -1,6 +1,11 @@
 const prisma = require("../utils/prisma");
 
-const getAllProductsDb = async () => await prisma.product.findMany();
+const getAllProductsDb = async () =>
+  await prisma.product.findMany({
+    include: {
+      category: true,
+    },
+  });
 
 module.exports = {
   getAllProductsDb,
