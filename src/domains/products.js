6172000e41ee0c7147e6a905136182e366ce698e
Kye <NovusAa12@proton.me>
2024-03-04 16:16:09 +0000

    refactor product domain to class with constructor and static methods

--- src/domains/products.js
+++ src/domains/products.js
@@ -1,19 +1,70 @@
 const prisma = require("../utils/prisma");
 
-const getAllProductsDb = async () =>
-  await prisma.product.findMany({
-    include: {
-      category: true,
-    },
-  });
+class Product {
+  constructor(
+    id = null,
+    title = null,
+    description = null,
+    price = null,
+    imageURL = null,
+    category = null
+  ) {
+    (this.id = id),
+      (this.title = title),
+      (this.description = description),
+      (this.price = price),
+      (this.imageURL = imageURL),
+      (this.category = category);
+  }
 
-const getProductByIdDb = async (id) =>
-  await prisma.product.findUnique({
-    where: { id },
-    include: { category: true },
-  });
+  static fromDb(product) {
+    return new Product(
+      product.id,
+      product.title,
+      product.description,
+      product.price,
+      product.imageURL,
+      product.category
+    );
+  }
 
-module.exports = {
-  getAllProductsDb,
-  getProductByIdDb,
-};
+  static async _findMany() {
+    return prisma.product.findMany({
+      include: {
+        category: true,
+      },
+    });
+  }
+
+  static async _findUniqueWhere(key, value) {
+    return prisma.product.findUnique({
+      where: {
+        [key]: value,
+      },
+      include: {
+        category: true,
+      },
+    });
+  }
+
+  static async getAllProducts() {
+    const foundProducts = await Product._findMany();
+    const allProducts = foundProducts.map(Product.fromDb);
+
+    return allProducts;
+  }
+
+  static async getProductById(id) {
+    const foundProduct = await Product._findUniqueWhere("id", id);
+
+    if (!foundProduct) {
+      return false;
+    }
+
+    const product = Product.fromDb(foundProduct);
+
+    return product;
+  }
+}
+
+module.exports = Product;
