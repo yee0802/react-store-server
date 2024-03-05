0d62c967929959b61f9e010cf938ef9707dd3d86
Kye <NovusAa12@proton.me>
2024-03-05 16:16:23 +0000

    add getCategoryById controller

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -10,3 +10,26 @@ export const getAllCategories = async (req, res) => {
     return res.status(e.status ?? 500).send({ error: e.message });
   }
 };
+
+export const getCategoryById = async (req, res) => {
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    return res
+      .status(401)
+      .send({ error: "Invalid ID: Please provide a valid number" });
+  }
+
+  try {
+    const foundCategory = await Category.getCategoryById(id);
+
+    if (!foundCategory) {
+      return res.status(404).send({ error: "No category found with given ID" });
+    }
+
+    return res.status(200).send({ category: foundCategory });
+  } catch (e) {
+    console.log("Error getting category by ID:", e.message);
+    return res.status(e.status ?? 500).send({ error: e.message });
+  }
+};
