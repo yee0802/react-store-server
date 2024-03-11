cac0e26ff942627220a0538639d66454f131f3af
Kye <NovusAa12@proton.me>
2024-03-11 16:29:01 +0000

    add user/login route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createUser } from "../controllers/user.js";
+import { login } from "../controllers/login.js";
 
 const router = Router();
 
 router.post("/register", createUser);
+router.post("/login", login);
 
 export default router;
