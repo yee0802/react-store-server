17d962d52a10019191eedff9c2d966111e3bd75f
Kye <NovusAa12@proton.me>
2024-03-14 18:56:10 +0000

    add /user/:id PATCH endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createUser, getUserById } from "../controllers/user.js";
+import {
+  createUser,
+  getUserById,
+  updateUserById,
+} from "../controllers/user.js";
 import { login } from "../controllers/login.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = Router();
 
 router.get("/:id", verifyToken, getUserById);
+router.patch("/:id", verifyToken, updateUserById);
 
 router.post("/register", createUser);
 router.post("/login", login);
